a7749446531315d0c16be9552fb8621f419ed618
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2025-07-07 20:07:51 +0530

    insertion and deletion of Node in LinkedList

--- LinkedList/DoublyLL.java
+++ LinkedList/DoublyLL.java
@@ -78,6 +78,77 @@ class DoublyLinkedList{
         }
         System.out.println("null");
     }
+
+
+    public void deleteAtBeginning(){
+        if(head==null){
+            System.out.println("Linkedlist is empty");
+            return;
+        }
+
+        if(head.next==null){
+            head=null;
+            return;
+        }
+
+        head=head.next;
+        head.next.prev=null;
+    }
+
+    public void deleteAtEnd(){
+        if(head==null){
+            System.out.println("LinkedList is empty");
+            return;
+        }
+
+        if(head.next==null){
+            head=null;
+            return;
+        }
+
+        Node temp=head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        temp.next.prev=null;
+    }
+
+    public void deleteAtPosition(int position){
+        if(head==null){
+            System.out.println("LinkedList is empty");
+            return;
+        }
+
+        if(position==1){
+            if(head.next==null){
+                head=null;
+                return;
+            }
+            head=head.next;
+            head.prev=null;
+            return;
+        }
+
+        Node temp=head;
+        for(int i=1;i<position-1;i++){
+            if(temp==null){
+                System.out.println("position is out of bound");
+                return;
+            }
+            temp=temp.next;
+        }
+        Node deleteNode=temp.next;
+        if(deleteNode==null){
+            deleteNode.prev.next=null;
+        }
+        else{
+            deleteNode.prev.next=deleteNode.next;
+            deleteNode.next.prev=deleteNode.prev;
+        }
+    }
+
+
 }
 
 public class DoublyLL {
@@ -87,7 +158,9 @@ public class DoublyLL {
         d1.insertAtEnd(2);
         d1.insertAtEnd(3);
         d1.insertAtEnd(4);
+        d1.deleteAtPosition(2);
         d1.display();
+
     }
     
 }
