31fdd08a02d1f81abe9bc64b7b9abb51e67c342d
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2025-08-06 00:17:11 +0530

    Implemented a code to find the height of tree

---
+++ BinaryTree/HeightOfTree.java
@@ -0,0 +1,47 @@
+package BinaryTree;
+
+public class HeightOfTree {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static int heightOfTree(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftheight=heightOfTree(root.left);
+        int rightheight=heightOfTree(root.right);
+
+        return Math.max(leftheight,rightheight)+1;
+
+    }
+    public static void main(String[] args) {
+        /*
+                1
+               / \
+              2   3
+            /  \   \
+            4   5   6
+
+         */
+        
+
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.right=new Node(6);
+
+        System.out.println("Height of tree is:"+heightOfTree(root));
+    }
+    
+}
