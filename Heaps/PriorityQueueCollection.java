a8efc0941066307f2265f98b2206deba0618f1fd
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2025-10-01 21:03:08 +0530

    Priority Queue implementation in Java collection

---
+++ Heaps/PriorityQueueCollection.java
@@ -0,0 +1,21 @@
+package Heaps;
+
+import java.util.PriorityQueue;;
+
+public class PriorityQueueCollection {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.add(5);
+        pq.add(3);
+        pq.add(1);
+        pq.add(4);
+        pq.add(2);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+        
+    }
+    
+}
