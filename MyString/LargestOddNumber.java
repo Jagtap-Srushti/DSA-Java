7de58d51424b8bca1f4889a9ffb57d34abced886
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-01-21 23:30:48 +0530

    Implemented code to remove outer most parentheses

--- MyString/LargestOddNumber.java
+++ MyString/LargestOddNumber.java
@@ -1,21 +1,23 @@
 package MyString;
+
 public class LargestOddNumber {
 
-    public static String largestOddString(String str){
-        for(int i=str.length()-1;i>=0;i--){
-            int digit=str.charAt(i)-'0';
+    public static String largestOddString(String str) {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            int digit = str.charAt(i) - '0';
 
-            if(digit%2!=0){         // Check if the digit is odd
+            if (digit % 2 != 0) { // Check if the digit is odd
                 // If odd, return substring from start to this index
-                return str.substring(0, i+1);       
+                return str.substring(0, i + 1);
             }
         }
 
         // If odd, return substring from start to this index
         return "";
     }
+
     public static void main(String[] args) {
-        String str="3542";
+        String str = "3542";
 
         System.out.println(largestOddString(str));
     }
---
+++ MyString/RemoveOuterParenthesis.java
@@ -0,0 +1,31 @@
+package MyString;
+public class RemoveOuterParenthesis {
+    public static String removeOuterParenthesis(String s){
+        StringBuilder sb=new StringBuilder();
+        int depth=0;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                if(depth>0){
+                    sb.append(ch);
+                }
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth>0){
+                    sb.append(ch);
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+    public static void main(String[] args) {
+        String s="((()))(())";
+
+        System.out.println(removeOuterParenthesis(s));
+    }
+}
