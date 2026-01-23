14670ad2b961c1ea0e00527dfdf8d940248f77e8
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-01-24 00:00:06 +0530

    Implemented code to find the Maximum nested depth of the parentheses

---
+++ MyString/MaximumNestedParentheses.java
@@ -0,0 +1,29 @@
+package MyString;
+
+public class MaximumNestedParentheses {
+
+    public static int maximumNestedParenthesis(String s){
+        int p=0;
+        int ans=0;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(ch=='('){
+                p++;
+            }
+            else if(ch==')'){
+                p--;
+            }
+            ans=Math.max(ans,p);
+        }
+
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        String s="(1+(2*3)+((8)/4))+1";
+        System.out.println(maximumNestedParenthesis(s));
+    }
+    
+}
