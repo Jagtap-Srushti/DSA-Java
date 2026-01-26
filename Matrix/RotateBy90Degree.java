e32bf6e949838f20b17a72e7477c8591afea3675
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-01-26 19:56:28 +0530

    Implemented ac ode to rotate the array by 90 degree clockwise

---
+++ Matrix/RotateBy90Degree.java
@@ -0,0 +1,55 @@
+package Matrix;
+public class RotateBy90Degree {
+    public static void rotateBy90Degree(int matrix[][]){
+        int n=matrix.length;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            int start=0,end=n-1;
+            while(start<end){
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void printMatrix(int matrix[][]){
+        int n=matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Matrix before rotation");
+
+        printMatrix(matrix);
+
+        System.out.println("Matrix after rotation");
+
+        rotateBy90Degree(matrix);
+
+        printMatrix(matrix);
+    }
+    
+}
