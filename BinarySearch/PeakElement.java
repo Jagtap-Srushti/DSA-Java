4821a104d23fb906e0f78ddf49a85ffca03bd5e4
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-03-16 23:30:49 +0530

    Implemented Code to find peak element in array

--- BinarySearch/PeakElement.java
+++ BinarySearch/PeakElement.java
@@ -1,5 +1,7 @@
 package BinarySearch;
 
+// Given an array of length N, peak element is defined as the element greater than both of its neighbors. Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i].
+
 public class PeakElement {
     public static int peakElementOptimal(int arr[]){
         int n=arr.length;
@@ -13,9 +15,41 @@ public class PeakElement {
         }
         return -1;
     }
+
+    public static int peakElement(int arr[]){
+        int n=arr.length;
+        if(n==1){
+            return 0;
+        }
+
+        if(arr[0]>arr[1]){
+            return 0;
+        }
+
+        if(arr[n-1]>arr[n-2]){
+            return n-1;
+        }
+
+        int low=0;
+        int high=n-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid-1]){           //mid is on left increasing slope and peak is on right
+                low=mid+1;
+            }
+            else{
+                high=mid-1;                        //mid is on right decreasing slope and peak is on left
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,4,3,2};
-        System.out.println(peakElementOptimal(arr));
+        int arr[]={1,2,3,4,5,7,1,2};
+        System.out.println(peakElement(arr));
     }
     
 }
