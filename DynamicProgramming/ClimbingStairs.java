4f7935bf308487ea50adb483bbe8d01960e59da4
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-04-15 19:53:52 +0530

    Solve Climbing Stairs problem using Tabulation Method

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -1,35 +1,54 @@
 package DynamicProgramming;
 
-
 // You are climbing a staircase. It takes n steps to reach the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
 import java.util.*;
+
 public class ClimbingStairs {
-    public static int climbStairs(int n,int ways[]){
-        if(n==0){
+
+    //Memoization
+    public static int climbStairsMemoization(int n, int ways[]) {
+        if (n == 0) {
             return 1;
         }
 
-        if(n==-1){
+        if (n == -1) {
             return 0;
         }
 
-        if(ways[n]!=-1){
+        if (ways[n] != -1) {
             return ways[n];
         }
 
-        ways[n]=climbStairs(n-1, ways)+climbStairs(n-2, ways);
+        ways[n] = climbStairsMemoization(n - 1, ways) + climbStairsMemoization(n - 2, ways);
         return ways[n];
     }
-    public static void main(String[] args) {
-        int n=5;
-        int ways[]=new int [n+1];
-        Arrays.fill(ways,-1);
 
-        System.out.println(climbStairs(n, ways));
-        
+    //Tabulation
+    public static int climbStairsTabulation(int n){
+        int dp[]=new int [n+1];
+
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                dp[i]=dp[i-1];
+            }
+            else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+
+        return dp[n];
     }
-    
+
+    public static void main(String[] args) {
+        int n = 5;
+        int ways[] = new int[n + 1];
+        Arrays.fill(ways, -1);
+
+        System.out.println(climbStairsTabulation(n));
+
+    }
+
 }
