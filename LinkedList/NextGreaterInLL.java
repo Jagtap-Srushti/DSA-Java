708c26a5ec8d4b2d7390f787288e730b4138c4ab
Jagtap-Srushti <srushti.jagtap136@gmail.com>
2026-09-20 00:13:25 +0530

    Solved Leetcode problem 1019. Next Greater Element in LinkedList

---
+++ LinkedList/NextGreaterInLL.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> list=new ArrayList<>();
+
+        ListNode temp=head;
+        while(temp!=null){
+            list.add(temp.val);
+            temp=temp.next;
+        }
+
+        int ans[]=new int[list.size()];
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=list.size()-1;i>=0;i--){
+            while(!st.empty() && st.peek()<=list.get(i)){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ans[i]=0;
+            }
+            else{
+                ans[i]=st.peek();
+            }
+            st.push(list.get(i));
+        }
+
+        return ans;
+    }
+}
